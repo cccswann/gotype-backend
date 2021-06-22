7b9889d9d50a6920348df1e3d59776463cdc0a90
root <cccswann@gmail.com>
2021-06-22 19:05:11 +0000

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :decks, only: [:index, :create]
-      resources :cards, only: [:index, :show]
+      resources :decks
+      resources :cards
     end
   end
 end
